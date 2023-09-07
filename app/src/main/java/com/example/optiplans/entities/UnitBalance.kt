fd83d31bb09b420df05e7546d7d9174df2a37901
DmitryPisarevskiy <f_o_r_w_a_r_d@inbox.ru>
2023-09-07 07:18:30 +0500

    corrected logic

--- app/src/main/java/com/example/optiplans/entities/UnitBalance.kt
+++ app/src/main/java/com/example/optiplans/entities/UnitBalance.kt
@@ -2,6 +2,7 @@ package com.example.optiplans.entities
 
 class UnitBalance {
     var feeds: MutableList<Stream> = mutableListOf()
-    var isEqual: Boolean
+    var products: MutableList<Stream> = mutableListOf()
+    var isEqual: Boolean = false
 
 }

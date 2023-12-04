1fa08414aecd67ff02700ca0d35c323e7982c071
DmitryPisarevskiy <f_o_r_w_a_r_d@inbox.ru>
2023-12-04 06:48:24 +0500

    corrected logic

--- app/src/main/java/com/example/optiplans/entities/Unit.kt
+++ app/src/main/java/com/example/optiplans/entities/Unit.kt
@@ -1,8 +1,26 @@
 package com.example.optiplans.entities
 
-data class Unit(var name: String, var tag: String){
+data class Unit(var name: String, var tag: String, val numOfPeriods: Int){
     var regimes: MutableList<Column> = mutableListOf()
     var capacities: MutableList<Capacity> = mutableListOf()
-    var unitBalance: UnitBalance = UnitBalance(this)
+    var isBalanced: Boolean = false
+    var feeds: MutableMap<Stream, Array<Float>> = mutableMapOf()
+    var products: MutableMap<Stream, Array<Float>> = mutableMapOf()
+    var isEqual: Boolean = false
+    var feedSum: Float=0f
+    var productSum: Float=0f
+
+    fun balance() {
+        regimes.forEach {
+            for (m in it.strAndCoeffs) {
+                if (m.value>0) {
+                    if (!feeds.containsKey(m.key)) {
+                        feeds.put(m.key,m.value*it.activities)
+                    }
+                }
+            }
+        }
+        feeds = regimes
+    }
 }
 
--- app/src/main/java/com/example/optiplans/logic/UnitLogic.kt
+++ app/src/main/java/com/example/optiplans/logic/UnitLogic.kt
@@ -1,9 +1,5 @@
 package com.example.optiplans.logic
 
-import com.example.optiplans.entities.UnitBalance
-
 class UnitLogic {
-    fun getUnitBalance(val u: Unit): UnitBalance() {
-        UnitBalance(unit = u)
-    }
+
 }

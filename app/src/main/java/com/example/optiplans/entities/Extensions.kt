a9cfb53288802b9e006ce852b37350667a4e2a81
DmitryPisarevskiy <f_o_r_w_a_r_d@inbox.ru>
2023-12-07 07:15:07 +0500

    Corrected Unit balance

--- app/src/main/java/com/example/optiplans/entities/Extensions.kt
+++ app/src/main/java/com/example/optiplans/entities/Extensions.kt
@@ -1,11 +1,17 @@
 package com.example.optiplans.entities
 
-class Extensions {
-    public operator fun Array<Float>.times(value: Float): Array<Float> {
-        val result = Array<Float> (this.size) {0f}
-        for (i in 0..<this.size) {
-            result[i] = this[i]*2
-        }
-        return result
+operator fun Array<Float>.times(value: Float): Array<Float> {
+    val result = Array<Float> (this.size) {0f}
+    for (i in 0..<this.size) {
+        result[i] = this[i]*value
     }
+    return result
+}
+
+operator fun Array<Float>.plus(array: Array<Float>): Array<Float> {
+    val result = Array<Float> (this.size) {0f}
+    for (i in 0..<this.size) {
+        result[i] = this[i]+array[i]
+    }
+    return result
 }
--- app/src/main/java/com/example/optiplans/entities/Unit.kt
+++ app/src/main/java/com/example/optiplans/entities/Unit.kt
@@ -1,50 +1,38 @@
 package com.example.optiplans.entities
 
-import com.example.optiplans.entities.Extensions
-
 class Unit(var name: String, var tag: String, val numOfPeriods: Int){
     var regimes: MutableList<Column> = mutableListOf()
     var capacities: MutableList<Capacity> = mutableListOf()
     var isBalanced: Boolean = false
     var feeds: MutableMap<Stream, Array<Float>> = mutableMapOf()
     var products: MutableMap<Stream, Array<Float>> = mutableMapOf()
-    var isEqual: Boolean = false
-    var feedSum: Float=0f
-    var productSum: Float=0f
+
+    var isEqualAll: Boolean = false
+    var feedSum: Array<Float> = Array<Float> (numOfPeriods) {0f}
+    var productSum: Array<Float> = Array<Float> (numOfPeriods) {0f}
 
     fun balance() {
         regimes.forEach {
             for (m in it.strAndCoeffs) {
                 if (m.value>0) {
                     if (!feeds.containsKey(m.key)) {
-                        feeds.put(m.key,it.activities.times(m.value))
+                        feeds[m.key] = it.activities.times(m.value)
                     } else {
                         feeds[m.key]?.plus(it.activities.times(m.value))
                     }
                 } else if (m.value<0) {
                     if (!products.containsKey(m.key)) {
-                        products.put(m.key,it.activities.times(m.value))
+                        products[m.key] = it.activities.times(m.value)
                     } else {
                         products[m.key]?.plus(it.activities.times(m.value))
                     }
                 }
             }
         }
-    }
-    operator fun Array<Float>.times(value: Float): Array<Float> {
-        val result = Array<Float> (this.size) {0f}
-        for (i in 0..<this.size) {
-            result[i] = this[i]*value
-        }
-        return result
-    }
+        feedSum = Array<Float> (numOfPeriods) {0f}
+        feeds.forEach {
 
-    operator fun Array<Float>.plus(array: Array<Float>): Array<Float> {
-        val result = Array<Float> (this.size) {0f}
-        for (i in 0..<this.size) {
-            result[i] = this[i]+array[i]
         }
-        return result
     }
 }
 

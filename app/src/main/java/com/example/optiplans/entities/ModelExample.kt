7d5c95d01d4f28be00c09a22f8bf28aaf01894b5
DmitryPisarevskiy <f_o_r_w_a_r_d@inbox.ru>
2024-02-06 21:27:42 +0500

    Corrected Unit balance

--- app/src/main/java/com/example/optiplans/entities/ModelExample.kt
+++ app/src/main/java/com/example/optiplans/entities/ModelExample.kt
@@ -8,16 +8,18 @@ object ModelExample : Model(3) {
         this.periods[2] = 11
 
         // Stream construction
-        val epUnit:Unit = Unit("ЭП-300", "MEP", 3)
-        val ethaneStream: Stream = Stream("Этан", "ETN", 3, bought = true)
-        val lpgStream: Stream = Stream("ШФЛУ", "SLU", 3, bought = true)
-        val naphtaStream: Stream = Stream("Нафта", "NAF", 3, bought = true)
-        val ethyleneStream: Stream = Stream("Этилен", "ETY", 3, sold = true)
-        val propyleneStream: Stream = Stream("Пропилен", "PRP", 3)
-        val tspStream: Stream = Stream("Пироконденсат", "PIR", 3, sold = true)
-        val hdpeStream: Stream = Stream("ПЭНД", "PND", 3, sold = true)
-        val iaaStream: Stream = Stream("ЛАК", "LAK", 3, sold = true)
-        val synGasStream: Stream = Stream("Синтез-газ", "SGA", 3, bought = true)
+        val epUnit = Unit("ЭП-300", "MEP", 3)
+        val hdpeUnit = Unit("ПЭНД", "PND", 3)
+        val iaaUnit = Unit("ЛАК", "PND", 3)
+        val ethaneStream = Stream("Этан", "ETN", 3, bought = true)
+        val lpgStream = Stream("ШФЛУ", "SLU", 3, bought = true)
+        val naphtaStream = Stream("Нафта", "NAF", 3, bought = true)
+        val ethyleneStream = Stream("Этилен", "ETY", 3, sold = true)
+        val propyleneStream = Stream("Пропилен", "PRP", 3)
+        val tspStream = Stream("Пироконденсат", "PIR", 3, sold = true)
+        val hdpeStream = Stream("ПЭНД", "PND", 3, sold = true)
+        val iaaStream = Stream("ЛАК", "LAK", 3, sold = true)
+        val waterStream = Stream("Синтез-газ", "SGA", 3, bought = true)
 
         // Prices, costs
         ethaneStream.costs = arrayOf(20f,20f,20f)
@@ -27,7 +29,7 @@ object ModelExample : Model(3) {
         tspStream.prices = arrayOf(10f,10f,10f)
         hdpeStream.prices = arrayOf(40f,40f,40f)
         iaaStream.prices = arrayOf(10f,10f,10f)
-        synGasStream.costs = arrayOf(10f,10f,10f)
+        waterStream.costs = arrayOf(10f,10f,10f)
 
         // Stream construction
         val etnColumn:Column = Column("Переработка этана", "ETN", 3)
@@ -49,12 +51,29 @@ object ModelExample : Model(3) {
         nafColumn.strAndCoeffs.put(ethyleneStream, -0.6f)
         nafColumn.strAndCoeffs.put(propyleneStream, -0.2f)
         nafColumn.strAndCoeffs.put(tspStream, -0.2f)
+        etyColumn.strAndCoeffs.put(ethyleneStream, 1.0f)
+        etyColumn.strAndCoeffs.put(hdpeStream, -1.0f)
+        prlColumn.strAndCoeffs.put(propyleneStream, 0.54f)
+        prlColumn.strAndCoeffs.put(waterStream, 0.46f)
+        prlColumn.strAndCoeffs.put(iaaStream, -1.0f)
 
         // Adding columns to units
         epUnit.regimes.add(etnColumn)
         epUnit.regimes.add(sluColumn)
         epUnit.regimes.add(nafColumn)
+        hdpeUnit.regimes.add(etyColumn)
+        iaaUnit.regimes.add(prlColumn)
+
+        //Adding units to model
         this.units.add(epUnit)
-        val hdpeUnit: Unit = Unit("ПЭНД", "PND", 3)
+        this.units.add(hdpeUnit)
+        this.units.add(iaaUnit)
+
+        //Activities settings
+        etnColumn.activities = arrayOf(1f, 0f, 0f)
+        sluColumn.activities = arrayOf(1f, 1f, 0f)
+        nafColumn.activities = arrayOf(1f, 0f, 1f)
+        etyColumn.activities = arrayOf(2f, 0.5f, 0.5f)
+        prlColumn.activities = arrayOf(0.45f, 0.15f, 0.2f)
     }
 }
--- app/src/test/java/com/example/optiplans/ExampleUnitTest.kt
+++ app/src/test/java/com/example/optiplans/ExampleUnitTest.kt
@@ -1,8 +1,6 @@
 package com.example.optiplans
 
-import com.example.optiplans.entities.Column
-import com.example.optiplans.entities.Stream
-import com.example.optiplans.entities.Unit
+import com.example.optiplans.entities.ModelExample
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -15,39 +13,12 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun unit_balance_is_correct() {
-        var u:Unit = Unit("Unit", "SFCC", 3)
-        var s1: Stream = Stream("Этан", "ETN", 3)
-        var s2: Stream = Stream("ШФЛУ", "SLU", 3)
-        var s3: Stream = Stream("Нафта", "NAF", 3)
-        var s4: Stream = Stream("Этилен", "ETY", 3)
-        var s5: Stream = Stream("Пропилен", "PRP", 3)
-        var s6: Stream = Stream("Пироконденсат", "PIR", 3)
-        var c1:Column = Column("Первый режим", "AAA", 3)
-        var c2:Column = Column("Второй режим", "BBB", 3)
-        var c3:Column = Column("Третий режим", "ССС", 3)
-        c1.strAndCoeffs.put(s1, 1f)
-        c1.strAndCoeffs.put(s4, -0.8f)
-        c1.strAndCoeffs.put(s5, -0.1f)
-        c1.strAndCoeffs.put(s6, -0.1f)
-        c2.strAndCoeffs.put(s2, 1f)
-        c2.strAndCoeffs.put(s4, -0.7f)
-        c2.strAndCoeffs.put(s5, -0.15f)
-        c2.strAndCoeffs.put(s6, -0.15f)
-        c3.strAndCoeffs.put(s3, 1f)
-        c3.strAndCoeffs.put(s4, -0.6f)
-        c3.strAndCoeffs.put(s5, -0.2f)
-        c3.strAndCoeffs.put(s6, -0.2f)
-        c1.activities = arrayOf(1f, 0f, 0f)
-        c2.activities = arrayOf(1f, 1f, 0f)
-        c3.activities = arrayOf(1f, 0f, 1f)
-        u.regimes.add(c1)
-        u.regimes.add(c2)
-        u.regimes.add(c3)
-        u.streamBalance()
-        //assertEquals(u.feedSum[0],3f)
-        //assertArrayEquals(u.products[s4],arrayOf(-2.1f,-0.7f,-0.6f))
-        //assertArrayEquals(u.products[s5],arrayOf(-0.45f,-0.15f,-0.2f))
-        //assertArrayEquals(u.products[s6],arrayOf(-0.45f,-0.15f,-0.2f))
-        assertEquals(true, u.isEqualAll)
+        val model = ModelExample
+        for (i in 0..<model.units.size) {
+            model.units[i].streamBalance()
+        }
+        assertEquals(true, model.units[0].isEqualAll)
+        assertEquals(true, model.units[1].isEqualAll)
+        assertEquals(true, model.units[2].isEqualAll)
     }
 }

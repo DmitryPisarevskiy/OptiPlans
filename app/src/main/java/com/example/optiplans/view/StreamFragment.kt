58d0fffa056070047d1d6dd2c7bcbc59c4acba24
DmitryPisarevskiy <f_o_r_w_a_r_d@inbox.ru>
2024-03-25 22:14:12 +0500

    Drawing stream fragment

--- app/src/main/java/com/example/optiplans/view/StreamFragment.kt
+++ app/src/main/java/com/example/optiplans/view/StreamFragment.kt
@@ -2,11 +2,13 @@ package com.example.optiplans.view
 
 import android.os.Build
 import android.os.Bundle
+import android.text.Layout
 import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import com.example.optiplans.R
@@ -42,28 +44,37 @@ class StreamFragment : Fragment() {
         iMax--
         for (i in 0..iMax) {
             val tvPeriod = TextView(context)
-            tvPeriod.text = "Период " + i.toString() + System.lineSeparator() + ModelExample.periods[i].toString() + " дней"
+            tvPeriod.text = "Период " + (i+1).toString() + System.lineSeparator() + ModelExample.periods[i].toString() + " дней"
             tvPeriod.gravity = Gravity.CENTER
             tvPeriod.setTextAppearance(R.style.TableText)
-            binding.trStreamPeriod.addView(tvPeriod)
+            binding.trStreamSailsPeriod.addView(tvPeriod)
         }
-        currentStream?.minBoundsSales?.forEach {
-            val tvMIN = TextView(context)
-            tvMIN.text = it.toString()
-            tvMIN.gravity = Gravity.CENTER
-            binding.trStreamMin.addView(tvMIN)
+        currentStream?.minBoundsSales?.apply { addTableItem(binding.trStreamSailsMin , this)  }
+        currentStream?.maxBoundsSales?.apply { addTableItem(binding.trStreamSailsMax , this) }
+        currentStream?.prices?.apply { addTableItem(binding.trStreamSailsPrice, this ) }
+        currentStream?.sails?.apply { addTableItem(binding.trStreamSailsSolution, this ) }
+
+        for (i in 0..iMax) {
+            val tvPeriod = TextView(context)
+            tvPeriod.text = "Период " + (i+1).toString() + System.lineSeparator() + ModelExample.periods[i].toString() + " дней"
+            tvPeriod.gravity = Gravity.CENTER
+            tvPeriod.setTextAppearance(R.style.TableText)
+            binding.trStreamPurchasesPeriod.addView(tvPeriod)
         }
-        currentStream?.maxBoundsSales?.forEach {
-            val tvMAX = TextView(context)
-            tvMAX.text = it.toString()
-            tvMAX.gravity = Gravity.CENTER
-            binding.trStreamMax.addView(tvMAX)
-        }
-        currentStream?.prices?.forEach {
-            val tvPrice = TextView(context)
-            tvPrice.text = it.toString()
-            tvPrice.gravity = Gravity.CENTER
-            binding.trStreamPrice.addView(tvPrice)
+        currentStream?.minBoundsPurchases?.apply { addTableItem(binding.trStreamPurchasesMin , this)  }
+        currentStream?.maxBoundsPurchases?.apply { addTableItem(binding.trStreamPurchasesMax , this) }
+        currentStream?.costs?.apply { addTableItem(binding.trStreamPurchasesPrice, this ) }
+        currentStream?.purchases?.apply { addTableItem(binding.trStreamPurchasesSolution, this ) }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun addTableItem(layout: LinearLayout, values: Array<Float>) {
+        values.forEach {
+            val textView = TextView(context)
+            textView.text = it.toString()
+            textView.gravity = Gravity.CENTER
+            textView.setTextAppearance(R.style.TableText)
+            layout.addView(textView)
         }
     }
 

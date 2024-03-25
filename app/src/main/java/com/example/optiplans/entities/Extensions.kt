867e101b95e099e9271eb154797148c03d1f9b8a
DmitryPisarevskiy <f_o_r_w_a_r_d@inbox.ru>
2024-03-25 20:28:33 +0500

    Drawing stream fragment

--- app/src/main/java/com/example/optiplans/entities/Extensions.kt
+++ app/src/main/java/com/example/optiplans/entities/Extensions.kt
@@ -1,5 +1,14 @@
 package com.example.optiplans.entities
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.MarginLayoutParamsCompat
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
+import androidx.core.view.setMargins
+
 fun Array<Float>.times(value: Float): Array<Float> {
     val result = Array<Float> (this.size) {0f}
     for (i in 0..<this.size) {
@@ -14,4 +23,15 @@ fun Array<Float>.plus(array: Array<Float>): Array<Float> {
         result[i] = this[i]+array[i]
     }
     return result
+}
+
+fun View.setMargins(
+    left: Int = this.marginLeft,
+    top: Int = this.marginTop,
+    right: Int = this.marginRight,
+    bottom: Int = this.marginBottom,
+) {
+    val params = ViewGroup.MarginLayoutParams(ViewGroup.MarginLayoutParams.WRAP_CONTENT,ViewGroup.MarginLayoutParams.WRAP_CONTENT)
+    params.setMargins(0)
+    layoutParams = params
 }
--- app/src/main/java/com/example/optiplans/view/StreamFragment.kt
+++ app/src/main/java/com/example/optiplans/view/StreamFragment.kt
@@ -1,15 +1,18 @@
 package com.example.optiplans.view
 
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import com.example.optiplans.R
 import com.example.optiplans.databinding.FragmentStreamBinding
 import com.example.optiplans.entities.ModelExample
 import com.example.optiplans.entities.Stream
-import org.w3c.dom.Text
 
 class StreamFragment : Fragment() {
     var currentStream: Stream? = null
@@ -29,6 +32,7 @@ class StreamFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         currentStream = ModelExample.currentStream
@@ -38,22 +42,27 @@ class StreamFragment : Fragment() {
         iMax--
         for (i in 0..iMax) {
             val tvPeriod = TextView(context)
-            tvPeriod.text = "Период " + i.toString() + "(" + ModelExample.periods[i].toString() + " дней)"
+            tvPeriod.text = "Период " + i.toString() + System.lineSeparator() + ModelExample.periods[i].toString() + " дней"
+            tvPeriod.gravity = Gravity.CENTER
+            tvPeriod.setTextAppearance(R.style.TableText)
             binding.trStreamPeriod.addView(tvPeriod)
         }
         currentStream?.minBoundsSales?.forEach {
             val tvMIN = TextView(context)
             tvMIN.text = it.toString()
+            tvMIN.gravity = Gravity.CENTER
             binding.trStreamMin.addView(tvMIN)
         }
         currentStream?.maxBoundsSales?.forEach {
             val tvMAX = TextView(context)
             tvMAX.text = it.toString()
+            tvMAX.gravity = Gravity.CENTER
             binding.trStreamMax.addView(tvMAX)
         }
         currentStream?.prices?.forEach {
             val tvPrice = TextView(context)
             tvPrice.text = it.toString()
+            tvPrice.gravity = Gravity.CENTER
             binding.trStreamPrice.addView(tvPrice)
         }
     }
